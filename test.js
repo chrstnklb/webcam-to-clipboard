c791e6b14a96265fe33940db4aefecd90fc640b9
Christian <chrstnklb@gmail.com>
2022-01-18 00:03:11 +0100

    Some CSS and receiving the cam resolution

--- test.js
+++ test.js
@@ -1,38 +1,58 @@
-/** https://usefulangle.com/post/352/javascript-capture-image-from-camera
- * 
- */
-
-let camera_button = document.querySelector("#start-camera");
-let video = document.querySelector("#video");
-let click_button = document.querySelector("#click-photo");
+let cameraButton = document.querySelector("#activate-cam");
+let camVideo = document.querySelector("#cam-video");
+let copyCamVideoToClipboard = document.querySelector("#copy-to-clipboard");
 let canvas = document.querySelector("#canvas");
 
-camera_button.addEventListener("click", async function () {
+let camWidth, camHeight;
+
+cameraButton.addEventListener("click", async function () {
   let stream = await navigator.mediaDevices.getUserMedia({
     video: true,
     audio: false,
   });
-  video.srcObject = stream;
+
+  let stream_settings = stream.getVideoTracks()[0].getSettings();
+
+  // actual width & height of the camera video
+  camWidth = stream_settings.width;
+  camHeight = stream_settings.height;
+
+  console.log("camWidth :>> ", camWidth);
+  console.log("camHeight :>> ", camHeight);
+
+  camVideo.srcObject = stream;
 });
 
+copyCamVideoToClipboard.addEventListener("click", async function () {
+  canvas
+    .getContext("2d")
+    // .drawImage(image,   dx, dy, dWidth,       dHeight);
+    // .drawImage(camVideo, 0,  0, canvas.width, canvas.height);
+    .drawImage(
+      camVideo,
+      0,
+      0,
+      camWidth,
+      camHeight,
+      0,
+      0,
+      120,
+      120
+    );
+  // .drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
 
-click_button.addEventListener("click", async function () {
-  canvas.getContext("2d").drawImage(video, 0, 0, canvas.width, canvas.height);
-  let image_data_url = canvas.toDataURL("image/png");
-
-  // data url of the image
-  console.log(image_data_url);
+  let imageDataUrl = canvas.toDataURL("image/png");
 
   try {
-    const imgURL = image_data_url;
+    const imgURL = imageDataUrl;
     const data = await fetch(imgURL);
     const blob = await data.blob();
     await navigator.clipboard.write([
       new ClipboardItem({
-        [blob.type]: blob
-      })
+        [blob.type]: blob,
+      }),
     ]);
-    console.log('Image copied.');
+    console.log("Image copied.");
   } catch (err) {
     console.error(err.name, err.message);
   }
