baad17ec8b653a9cbd78b2336f51c5c67206c61a
Christian <chrstnklb@gmail.com>
2022-01-28 19:59:16 +0100

    Fix console log for local storage setting for confetti.

--- confetti.js
+++ confetti.js
@@ -3,7 +3,7 @@ let confettiSwitch = document.querySelector("#confettiSwitch");
 let localStorageFieldConfetti = "allowConfetti";
 
 // set confetti true on the first time the page is loaded and the localStorage is empty
-console.log(`Confetti is ${localStorage.getItem(localStorageFieldConfetti) ? "allowed" : "not allowed"}`);
+console.log(`Confetti is ${localStorage.getItem(localStorageFieldConfetti) === "true" ? "allowed" : "not allowed"}`);
 localStorage.setItem(
   localStorageFieldConfetti,
   localStorage.getItem(localStorageFieldConfetti) !== "false" // true when (true or null)
